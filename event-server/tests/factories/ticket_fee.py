f8ba1c4150de5fdfe574cdc9ee27942c4ae1f874
Ryan <niemiryan@gmail.com>
2020-07-09 10:59:45 -0700

    More commit

---
+++ event-server/tests/factories/ticket_fee.py
@@ -0,0 +1,14 @@
+import factory
+
+from app.models.ticket_fee import TicketFees, db
+from tests.factories import common
+from tests.factories.base import BaseFactory
+
+
+class TicketFeesFactory(BaseFactory):
+    class Meta:
+        model = TicketFees
+
+    currency = common.currency_
+    service_fee = common.fee_
+    maximum_fee = common.fee_
